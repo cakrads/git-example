1784bb8eed71dbb16f36879fac98e5388f87f7c8
smanrung23 <smanrung23@gmail.com>
2020-08-05 16:18:05 +0700

    add feature sopian

---
+++ person/sopian/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+  for ($i=1; $i <= 10 ; $i++) { 
+  	if($i == 2){
+  		echo "GOOD";
+  	}
+    if ($i == 4){
+  		echo "BEST";
+  	}
+  	if ($i == 8) {
+  		echo "AWESOME";
+  	} 
+
+  	echo $i." ";
+  }
+
+?>
+</body>
+</html>
