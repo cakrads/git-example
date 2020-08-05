af796076a9ba052cef19d63a8401744f4cd26c4d
NovencusSnbl <novencussinambela00@gmail.com>
2020-08-05 15:54:41 +0700

    add feature novencus

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+for ($i=1; $i <=10  ; $i++) { 
+	if ($i == 2) {
+		echo "GOOD ";
+	}
+	if ($i == 4) { 
+		echo "BEST ";
+	}
+	if ($i == 8) { 
+		echo "AWESOME ";
+	}
+	echo $i.' ';
+}
+
+?>
